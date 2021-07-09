40472a7ae6c3f30c3e2d6d670ea145c482516d18
ML Engineer <paulobatista.phd@gmail.com>
2021-07-09 07:10:08 -0300

    update

--- app-engine-flask/main.py
+++ app-engine-flask/main.py
@@ -8,7 +8,7 @@ app = flask.Flask(__name__)
 @app.route("/", methods=["GET"])
 def hello():
     """ Return a friendly HTTP greeting. """
-    return "Hello World!\n"
+    return "Bem vindos - app engine - v1\n"
 
 
 if __name__ == "__main__":
